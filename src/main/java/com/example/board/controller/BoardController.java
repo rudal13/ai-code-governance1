d656e1c6a6adea24b84dcf57ac68c6ec5906b865
rudal13 <kmlee13@sk.com>
2026-07-18 19:01:09 +0900

    revert: 시연용 테스트 커밋 되돌리기

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -23,7 +23,6 @@ public class BoardController {
 
     @GetMapping
     public ApiResponse<List<BoardVO>> getBoardList() {
-        System.out.println("오류테스트");
         return ApiResponse.success(boardService.getBoardList());
     }
 
